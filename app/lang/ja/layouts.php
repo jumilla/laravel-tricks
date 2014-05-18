2f2c382919febcbaef7f6dd2f033449f2d41918b
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2014-05-18 11:30:30 +0900

    add keywords

--- app/lang/ja/layouts.php
+++ app/lang/ja/layouts.php
@@ -4,5 +4,6 @@ return [
     'main' => [
     	'html_title_suffix' => 'Laravel Tricks',
         'description' => 'Laravel tricksはPHPフレームワーク Laravelのための有用なヒントやコツを集約した日本語訳サイトです',
+        'keywords' => 'laravel, php, framework, web, artisans, taylor otwell, 日本語、日本語ドキュメント',
     ]
 ];
